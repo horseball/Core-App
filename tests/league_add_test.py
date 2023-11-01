8c25670c728792ad9cf71966c4df87f03b9c3586
Manon Deloupy <mdeloupy@gmail.com>
2023-11-01 13:58:38 +0100

    Add 422 test

--- tests/league_add_test.py
+++ tests/league_add_test.py
@@ -6,8 +6,8 @@ from LeagueAdd import main
 from unittest import mock 
 
 class TestLeagueAdd(unittest.TestCase):
-    def test_league_add_success(self):
-        # Request 
+    def test_league_add_200(self):
+        # Setup
         request = func.HttpRequest(
             method='POST',
             url='api/leagues/create',
@@ -22,3 +22,17 @@ class TestLeagueAdd(unittest.TestCase):
         # Assertions
         assert response.status_code == 200
         assert "League successfully created. Name : Foobar, country : can." in response.get_body().decode()
+
+    def test_league_add_422(self):
+        # Setup 
+        request = func.HttpRequest(
+            method='POST',
+            url='api/leagues/create',
+            body=json.dumps({}).encode('utf8')
+        )
+
+        response = main(request)
+
+        # Assertions
+        assert response.status_code == 422
+        assert "Params are missing. Please include at least a 'name' to create a league." in response.get_body().decode()
